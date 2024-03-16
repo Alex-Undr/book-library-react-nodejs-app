5c20b20bbf8001fd187182a8bcae54b3ee77882c
Alex-Undr <golybua@gmail.com>
2024-03-16 16:12:47 +0200

    Added createAsyncThunk

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addBook, thunkFunction } from '../../redux/slices/bookSlices';
+import { addBook, fetchBook } from '../../redux/slices/bookSlices';
 import createBookWithID from '../../utils/createBookWithID';
 import booksData from '../../data/books.json';
 import './BookForm.css';
@@ -28,7 +28,7 @@ const BookForm = () => {
   };
 
   const handleAddRandomBookViaAPI = () => {
-    dispatch(thunkFunction);
+    dispatch(fetchBook());
   };
 
   return (
